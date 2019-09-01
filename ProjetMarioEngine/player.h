beb6118653afe700d0dc81746bfafc6ea3b78990
VictorCouturieux <victor.couturieux@sas-impact.fr>
2019-09-01 18:27:09 +0200

    comm deleted

--- ProjetMarioEngine/player.h
+++ ProjetMarioEngine/player.h
@@ -51,26 +51,3 @@ private:
 
 #endif // PLAYER_H
 
-
-//#ifndef PLAYER_H
-//#define PLAYER_H
-
-//#include <QGraphicsRectItem>
-//#include <QObject>
-
-//class Player: public QObject, public QGraphicsRectItem
-//{
-//Q_OBJECT
-//public:
-//    Player();
-//public:
-//    void keyPressEvent(QKeyEvent * event);
-//public:
-//    void keyReleaseEvent(QKeyEvent * event);
-//public slots:
-//    void goPlayer();
-
-//};
-
-//#endif // PLAYER_H
-
